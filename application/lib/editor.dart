47470305f25108da2bba1a55000a11b4309bfbec
Ian Hickson <ian@hixie.ch>
2020-10-20 13:54:22 -0700

    Allow name to be edited

--- application/lib/editor.dart
+++ application/lib/editor.dart
@@ -15,14 +15,92 @@ class Editor extends StatefulWidget {
 }
 
 class _EditorState extends State<Editor> {
-  TextEditingController _name;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(widget.atom.kindDescription, style: Theme.of(context).textTheme.headline4),
+            StringField(
+              label: 'Name',
+              value: widget.atom.name.value,
+              onChanged: (String value) { widget.atom.name.value = value; },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StringField extends StatefulWidget {
+  const StringField({
+    Key key,
+    this.label,
+    this.value,
+    this.onChanged,
+  }): super(key: key);
+
+  final String label;
+  final String value;
+  final ValueSetter<String> onChanged;
+
+  @override
+  State<StringField> createState() => _StringFieldState();
+}
+
+class _StringFieldState extends State<StringField> {
+  TextEditingController _controller;
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.value);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void didUpdateWidget(StringField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.value != _controller.text) {
+      _controller.text = widget.value;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: ListBody(
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('${widget.atom}'),
+          SizedBox(
+            width: 120.0,
+            child: InkWell(
+              onTap: () {
+                _focusNode.requestFocus();
+              },
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text('${widget.label}:'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                focusNode: _focusNode,
+                controller: _controller,
+                decoration: const InputDecoration(
+                  filled: true,
+                  border: InputBorder.none,
+                ),
+                onChanged: widget.onChanged,
+              ),
+            ),
+          ),
         ],
       ),
     );
