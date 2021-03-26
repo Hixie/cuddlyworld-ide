169f4811d8bc3721401cfa023f9524e3e5ca70d4
Ian Hickson <ian@hixie.ch>
2021-03-26 14:39:45 -0700

    update to recent flutter

--- application/lib/catalog.dart
+++ application/lib/catalog.dart
@@ -79,7 +79,7 @@ class _DraggableTextState extends State<DraggableText> {
     final EditorDisposition editor = EditorDisposition.of(context);
     return Draggable<Atom>(
       data: widget.atom,
-      dragAnchor: DragAnchor.pointer,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
       feedback: FractionalTranslation(
         translation: const Offset(-0.5, -0.5),
         child: Material(
@@ -92,8 +92,8 @@ class _DraggableTextState extends State<DraggableText> {
       ),
       child: Padding(
         padding: EdgeInsets.only(top: widget.atom.parent != null ? 0.0 : 8.0),
-        child: FlatButton(
-          color: widget.atom == editor.current ? Colors.yellow : null,
+        child: TextButton(
+          style: widget.atom == editor.current ? ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow)) : null,
           onPressed: () {
             setState(() {
               EditorDisposition.of(context).current = widget.atom;
